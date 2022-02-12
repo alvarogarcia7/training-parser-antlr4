35618a8fbc5dbba3d7d6f7d932a46c22517af952
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-02-12 23:05:15 +0400

    Refactor: order of parameters in a function

--- parser/test_parser.py
+++ parser/test_parser.py
@@ -11,27 +11,27 @@ class TestParser(unittest.TestCase):
         result = Parser.from_string('Bench press 10k: 4, 4x5\n').parse_sessions()
 
         self.assertListEqual(result,
-                             [Exercise('Bench press', [self.serie(10, 4)] + [self.serie(10, 5) for _ in range(5)])])
+                             [Exercise('Bench press', [self.serie(4, 10)] + [self.serie(5, 10) for _ in range(5)])])
 
     def test_visit_sessions_only_multiple(self) -> None:
         result = Parser.from_string('Squat 70k: 5x10').parse_sessions()
 
-        self.assertListEqual(result, [Exercise('Squat', [self.serie(10, 5) for _ in range(5)])])
+        self.assertListEqual(result, [Exercise('Squat', [self.serie(5, 10) for _ in range(5)])])
 
     def test_visit_sessions_multi_series_format(self) -> None:
         result = Parser.from_string('Overhead press 5x6x40k\n').parse_sessions()
 
-        self.assertListEqual(result, [Exercise('Overhead press', [self.serie(40, 6) for _ in range(5)])])
+        self.assertListEqual(result, [Exercise('Overhead press', [self.serie(6, 40) for _ in range(5)])])
 
     def test_visit_sessions_multi_singles_format(self) -> None:
         result = Parser.from_string('Deadlift 60k: 20, 15,8,8\n').parse_sessions()
 
-        self.assertListEqual(result, [Exercise('Deadlift', [self.serie(60, i) for i in [20, 15, 8, 8]])])
+        self.assertListEqual(result, [Exercise('Deadlift', [self.serie(i, 60) for i in [20, 15, 8, 8]])])
 
     def test_visit_sessions_another_exercise(self) -> None:
         result = Parser.from_string('Row en maquina 41k: 15, 8\n').parse_sessions()
 
-        self.assertListEqual(result, [Exercise('Row en maquina', [self.serie(41, i) for i in [15, 8]])])
+        self.assertListEqual(result, [Exercise('Row en maquina', [self.serie(i, 41) for i in [15, 8]])])
 
-    def serie(self, amount: int, repetition: int) -> Repetition:
+    def serie(self, repetition: int, amount: int) -> Repetition:
         return {'repetitions': repetition, 'weight': {'amount': amount, 'unit': Units.KILOGRAM}}
