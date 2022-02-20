48291ca03b471338b1b93a8d88fbb2eb0eb339e0
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2022-02-20 10:32:03 +0400

    Green: colon is optional

--- parser/test_parser.py
+++ parser/test_parser.py
@@ -12,10 +12,21 @@ class TestParser(unittest.TestCase):
 
         self.assertListEqual(result, [(Exercise('Bench press', [self.serie(i, 10) for i in [4] + 4 * [5]]))])
 
+    def test_visit_sessions_mixing_single_and_multiple_no_colon(self) -> None:
+        result = Parser.from_string('Bench press 10k 4, 4x5\n').parse_sessions()
+
+        self.assertListEqual(result, [(Exercise('Bench press', [self.serie(i, 10) for i in [4] + 4 * [5]]))])
+
     def test_visit_sessions_only_multiple(self) -> None:
         result = Parser.from_string('Squat 70k: 5x10\n').parse_sessions()
 
         self.assertListEqual(result, [Exercise('Squat', [self.serie(10, 70) for _ in range(5)])])
+
+    def test_visit_sessions_only_multiple_no_colon(self) -> None:
+        result = Parser.from_string('Squat 70k 5x10\n').parse_sessions()
+
+        self.assertListEqual(result, [Exercise('Squat', [self.serie(10, 70) for _ in range(5)])])
+
 
     def test_visit_sessions_multi_series_format(self) -> None:
         result = Parser.from_string('Overhead press 5x6x40k\n').parse_sessions()
