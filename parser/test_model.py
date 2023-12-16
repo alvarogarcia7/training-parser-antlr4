7be3e7d2d33170b715ab7e3694a7bc6726ed303c
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2023-12-17 01:22:52 +0400

    Green: add a test for actually groupby repetitions and weight

--- parser/test_model.py
+++ parser/test_model.py
@@ -14,30 +14,51 @@ class TestModel(unittest.TestCase):
             Exercise('b', [{'repetitions': repetition, 'weight': {'amount': amount, 'unit': Units.KILOGRAM}}]),
             Exercise('b', []))
 
+    def test_exercise_group_by_weight_and_repetitions__should_flatten_when_all_match(self) -> None:
+        unflattened = Exercise('name',
+                               [{'repetitions': 10, 'weight': {'amount': 40.0, 'unit': 'kg'}},
+                                {'repetitions': 10, 'weight': {'amount': 40.0, 'unit': 'kg'}},
+                                {'repetitions': 6, 'weight': {'amount': 40.0, 'unit': 'kg'}}])
+        flattened_list_by_weight_and_repetitions = [
+            Exercise('name', [
+                {'repetitions': 10, 'weight': {'amount': 40.0, 'unit': 'kg'}},
+                {'repetitions': 10, 'weight': {'amount': 40.0, 'unit': 'kg'}}
+            ]),
+            Exercise('name', [{'repetitions': 6, 'weight': {'amount': 40.0, 'unit': 'kg'}}])
+        ]
 
+        self.assertEqual(unflattened.flatten(), flattened_list_by_weight_and_repetitions)
 
-
-
-    def test_exercise_group_by_weight(self) -> None:
+    def test_exercise_group_by_weight_requires_exact_repetitions_and_weight(self) -> None:
         unflattened = Exercise('name',
                                [{'repetitions': 50, 'weight': {'amount': 20.0, 'unit': 'kg'}},
                                 {'repetitions': 15, 'weight': {'amount': 40.0, 'unit': 'kg'}},
                                 {'repetitions': 6, 'weight': {'amount': 60.0, 'unit': 'kg'}}])
-        flattened_list_by_weight = [
+        flattened_list_by_weight_and_repetitions = [
             Exercise('name', [{'repetitions': 50, 'weight': {'amount': 20.0, 'unit': 'kg'}}]),
             Exercise('name', [{'repetitions': 15, 'weight': {'amount': 40.0, 'unit': 'kg'}}]),
             Exercise('name', [{'repetitions': 6, 'weight': {'amount': 60.0, 'unit': 'kg'}}])
         ]
 
-        self.assertEqual(unflattened.flatten(), flattened_list_by_weight)
+        self.assertEqual(unflattened.flatten(), flattened_list_by_weight_and_repetitions)
 
-    def test_exercise_group_by_weight_and_repetitions(self) -> None:
+    def test_exercise_group_by_weight_and_repetitions_requires_exact_repetitions(self) -> None:
         unflattened = Exercise('name',
                                [{'repetitions': 10, 'weight': {'amount': 40.0, 'unit': 'kg'}},
                                 {'repetitions': 6, 'weight': {'amount': 40.0, 'unit': 'kg'}}])
-        flattened_list_by_weight = [
+        flattened_list_by_weight_and_repetitions = [
             Exercise('name', [{'repetitions': 10, 'weight': {'amount': 40.0, 'unit': 'kg'}}]),
             Exercise('name', [{'repetitions': 6, 'weight': {'amount': 40.0, 'unit': 'kg'}}])
+        ]
+        self.assertEqual(unflattened.flatten(), flattened_list_by_weight_and_repetitions)
+
+    def test_exercise_group_by_weight_and_repetitions_requires_exact_weight(self) -> None:
+        unflattened = Exercise('name',
+                               [{'repetitions': 10, 'weight': {'amount': 40.0, 'unit': 'kg'}},
+                                {'repetitions': 10, 'weight': {'amount': 50.0, 'unit': 'kg'}}])
+        flattened_list_by_weight = [
+            Exercise('name', [{'repetitions': 10, 'weight': {'amount': 40.0, 'unit': 'kg'}}]),
+            Exercise('name', [{'repetitions': 10, 'weight': {'amount': 50.0, 'unit': 'kg'}}])
         ]
 
         self.assertEqual(unflattened.flatten(), flattened_list_by_weight)
