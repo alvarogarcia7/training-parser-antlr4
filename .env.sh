63b0bae35de66401ec07f1f653c0186712356994
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2026-04-20 14:16:56 +0400

    allow for java and uv not found

--- .env.sh
+++ .env.sh
@@ -2,9 +2,15 @@ if [ -f .venv/bin/activate ]; then
     source .venv/bin/activate
 fi
 
-if ! command java; then
+java >/dev/null 2>&1
+if [[ ! $? -eq 0 ]]; then
   echo "Java not found / not working. Overriding java"
   export PATH="/opt/homebrew/opt/openjdk@11/bin:$PATH"
+fi
+
+if ! command uv ; then
+  echo "uv not found or not working. Overriding uv"
+  export PATH="$PATH:$HOME/.local/bin"
 fi
 
 if [[ -f Makefile ]]; then
